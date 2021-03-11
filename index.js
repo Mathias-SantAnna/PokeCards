f7bbec3c5557218c22701ebae3e1e9b8579ccaff
Mathias-SantAnna <67033618+Mathias-SantAnna@users.noreply.github.com>
2021-03-11 23:24:35 +0000

    resetBoard function added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let firstCard, secondCard;
 
 function flipCard() {
     if (lockBoard) return;
+    if (this === firstCard) return;
 
     this.classList.add('flip');
 
@@ -41,8 +42,13 @@ function unflipCards() {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         
-        lockBoard = false;
+        resetBoard();
     }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));

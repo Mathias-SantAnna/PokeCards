b40c5c854b1169fe0c2dbe8f0883f46f0e51d478
Mathias-SantAnna <67033618+Mathias-SantAnna@users.noreply.github.com>
2021-03-11 23:07:50 +0000

    refactoring into ternary operator checkForMatch function

--- index.js
+++ index.js
@@ -19,17 +19,21 @@ function flipCard() {
 }
 
 function checkForMatch() {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        //Matches 
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-    } else {
-        // Doesn't Match
-        setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-        }, 1500)
-    }
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));

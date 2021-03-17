08b838ccdad48ef1f18fdb4127bb56f257732f18
Mathias-SantAnna <67033618+Mathias-SantAnna@users.noreply.github.com>
2021-03-17 08:57:36 +0000

    trying out modals

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,9 +23,22 @@ let scoreCounter = 0;
         this.victorySound.play();}
 } */
 
+//----- TRYING MODALS -----
+//IS THIS ONE=> 
+/*$(document).ready(function() {
+    setTimeout(function(){
+        $("#userInfoModal").modal('show');
+    },500);
+});
+//OR THIS ONE=> 
+setTimeout(function() {
+    $("#userInfoModal").modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+}, 500);*/
 
 function timer(){
-	
 	setInterval(function(){ 
 		timeCounter = timeCounter + 1;
 		document.getElementById('time-remaining').innerHTML = 'Timer: ' + timeCounter; 
@@ -33,7 +46,6 @@ function timer(){
  }, 1000);
 	
 }
-
 
 function flipCard() {
 	if (timeCounter ===0){

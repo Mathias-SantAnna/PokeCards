3430789bde81c27163ab814ce77c3a67064a0487
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2025-03-18 23:24:57 +0000

    add assets

--- poke_cards_kids/lib/main.dart
+++ poke_cards_kids/lib/main.dart
@@ -1,122 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(PokeFeedGame());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class PokeFeedGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokémon Feeding Game',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        fontFamily: 'Comic Sans',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PokeFeedScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PokeFeedScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PokeFeedScreenState createState() => _PokeFeedScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PokeFeedScreenState extends State<PokeFeedScreen> {
+  final Map<String, int> fruitInventory = {
+    'assets/fruits/apple.png': 10,
+    'assets/fruits/banana.png': 8,
+    'assets/fruits/berry.png': 5,
+    'assets/fruits/grape.png': 7,
+  };
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final String pokemonImage = 'assets/images/pikachu.png';
+  final String pokemonSound = 'assets/audio/pikachu.mp3';
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool isEating = false;
+
+  void _playSound(String audioPath) async {
+    await _audioPlayer.play(AssetSource(audioPath));
+  }
+
+  void _feedPokemon(String fruit) {
+    if (fruitInventory[fruit]! > 0) {
+      setState(() {
+        fruitInventory[fruit] = fruitInventory[fruit]! - 1;
+        isEating = true;
+      });
+      _playSound(pokemonSound);
+      Future.delayed(Duration(seconds: 1), () {
+        setState(() {
+          isEating = false;
+        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text('Feed Pikachu!')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: DragTarget<String>(
+                onAccept: (fruit) => _feedPokemon(fruit),
+                builder: (context, candidateData, rejectedData) {
+                  return Column(
+                    children: [
+                      Image.asset(
+                        pokemonImage,
+                        width: 200,
+                        height: 200,
+                      ),
+                      if (isEating)
+                        Text(
+                          'Yummy!',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.green),
+                        ),
+                    ],
+                  );
+                },
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: fruitInventory.length,
+              itemBuilder: (context, index) {
+                String fruit = fruitInventory.keys.elementAt(index);
+                return Draggable<String>(
+                  data: fruit,
+                  feedback: Image.asset(fruit, width: 50, height: 50),
+                  childWhenDragging: Opacity(
+                    opacity: 0.5,
+                    child: Image.asset(fruit, width: 50, height: 50),
+                  ),
+                  child: Column(
+                    children: [
+                      Image.asset(fruit, width: 50, height: 50),
+                      Text('x${fruitInventory[fruit]}', style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Back to Menu'),
+          ),
+          SizedBox(height: 20),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

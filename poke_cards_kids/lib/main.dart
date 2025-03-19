51886f98eaaaf91baac2abc633fa332ccb49a494
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2025-03-19 23:06:49 +0000

    flutter app branch geve up

--- poke_cards_kids/lib/main.dart
+++ poke_cards_kids/lib/main.dart
@@ -26,20 +26,27 @@ class PokeFeedScreen extends StatefulWidget {
 
 class _PokeFeedScreenState extends State<PokeFeedScreen> {
   final Map<String, int> fruitInventory = {
-    'assets/fruits/apple.png': 10,
-    'assets/fruits/banana.png': 8,
-    'assets/fruits/berry.png': 5,
-    'assets/fruits/grape.png': 7,
+    'assets/fruits/GoldenRazzBerry.png': 10,
+    'assets/fruits/NanabBerry.png': 8,
+    'assets/fruits/RazzBerry.png': 5,
+    'assets/fruits/PinapBerry.png': 7,
   };
 
   final String pokemonImage = 'assets/images/pikachu.png';
-  final String pokemonSound = 'assets/audio/pikachu.mp3';
+  final String pokemonSound = 'assets/audio/pikachu-pikapi.mp3';
   final AudioPlayer _audioPlayer = AudioPlayer();
   bool isEating = false;
 
   void _playSound(String audioPath) async {
-    await _audioPlayer.play(AssetSource(audioPath));
+    try {
+      final player = AudioPlayer();
+      await player.setSource(AssetSource(audioPath));
+      await player.play(AssetSource(audioPath)); // Use play instead of resume
+    } catch (e) {
+      print('Error playing sound: $e');
+    }
   }
+
 
   void _feedPokemon(String fruit) {
     if (fruitInventory[fruit]! > 0) {
@@ -71,8 +78,9 @@ class _PokeFeedScreenState extends State<PokeFeedScreen> {
                     children: [
                       Image.asset(
                         pokemonImage,
-                        width: 200,
-                        height: 200,
+                        width: 600,  // Increased size
+                        height: 600, // Increased size
+                        fit: BoxFit.contain,
                       ),
                       if (isEating)
                         Text(
@@ -86,7 +94,7 @@ class _PokeFeedScreenState extends State<PokeFeedScreen> {
             ),
           ),
           Container(
-            height: 100,
+            height: 120, // Slightly increased height for fruit selection
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: fruitInventory.length,
@@ -94,14 +102,14 @@ class _PokeFeedScreenState extends State<PokeFeedScreen> {
                 String fruit = fruitInventory.keys.elementAt(index);
                 return Draggable<String>(
                   data: fruit,
-                  feedback: Image.asset(fruit, width: 50, height: 50),
+                  feedback: Image.asset(fruit, width: 60, height: 60),
                   childWhenDragging: Opacity(
                     opacity: 0.5,
-                    child: Image.asset(fruit, width: 50, height: 50),
+                    child: Image.asset(fruit, width: 60, height: 60),
                   ),
                   child: Column(
                     children: [
-                      Image.asset(fruit, width: 50, height: 50),
+                      Image.asset(fruit, width: 60, height: 60),
                       Text('x${fruitInventory[fruit]}', style: TextStyle(fontSize: 18)),
                     ],
                   ),
@@ -111,10 +119,8 @@ class _PokeFeedScreenState extends State<PokeFeedScreen> {
           ),
           SizedBox(height: 20),
           ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Back to Menu'),
+            onPressed: ()  => _playSound('assets/audio/pikachu-pikapi.mp3'),
+            child: Text('test sound'),
           ),
           SizedBox(height: 20),
         ],
